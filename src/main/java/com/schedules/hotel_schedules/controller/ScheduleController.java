ac9fa2410d05a5d436c06129c5e3e813bc71eb24
ViniciusGavaPereira <vini-gava@outlook.com.br>
2024-08-07 21:17:59 -0300

    Criacao do endpoint createSchedule e populacao da classe ScheduleDTo

--- src/main/java/com/schedules/hotel_schedules/controller/ScheduleController.java
+++ src/main/java/com/schedules/hotel_schedules/controller/ScheduleController.java
@@ -3,15 +3,20 @@ package com.schedules.hotel_schedules.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 import com.schedules.hotel_schedules.dtos.ClientDto;
 import com.schedules.hotel_schedules.dtos.RoomDto;
+import com.schedules.hotel_schedules.dtos.ScheduleDto;
 import com.schedules.hotel_schedules.entities.Schedule;
 import com.schedules.hotel_schedules.service.ScheduleService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(value = "/schedule")
@@ -36,6 +41,15 @@ public class ScheduleController {
         // Connect to Room's endpoint
         RoomDto roomResult = scheduleService.findRoom(idRoom);
         return "Person: " + clientResult.toString() + "\nRoom: " + roomResult.toString();
+    }
+
+    @PostMapping("/create/v1/")
+    public ResponseEntity<ScheduleDto> createSchedule(@RequestBody Schedule scheduleInput) {
+
+        Schedule schedule = scheduleService.createSchedule(scheduleInput);
+
+        return new ResponseEntity<>(new ScheduleDto(schedule),
+                HttpStatus.CREATED);
     }
 
 }
